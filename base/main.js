d13e1065981f115dfb81a29e50acde89e6d02b8f
Le Codex <40335983+LeCodex@users.noreply.github.com>
2020-09-25 17:22:49 +0200

    Reworked structure

---
+++ base/main.js
@@ -0,0 +1,70 @@
+const fs = require('fs');
+
+class Base {
+  constructor(client) {
+    this.client = client;
+    this.name = "[TODO] Add name";
+    this.description = "[TODO] Add description";
+    this.help = {
+      "": "[TODO] Add help"
+    }
+    this.command_text = "";
+    this.color = 0xffffff;
+    this.auth = [];
+  }
+
+  _execCommand(message) {
+    if (message.content.startsWith(this.client.config.prefix) && !message.author.bot) {
+      var args = message.content.substring(this.client.config.prefix.length).split(" ");
+      var cmd = args.shift();
+      var kwargs = {};
+      for (var index = args.length - 1; index >= 0; index--) {
+        var element = args[index];
+        if (element.search(/\S+=\S+/) != -1) {
+          var key = element.match(/\S+=/)[0];
+          var value = element.match(/=\S+/)[0];
+          kwargs[key.substring(0, key.length - 1)] = value.substring(1);
+          args.splice(index, 1);
+        }
+      };
+
+      if (cmd == this.command_text) {
+        if (this.auth.length == 0 || this.auth.includes(message.author.id)) {
+          if (this["com_" + args[0]]) {
+            this["com_" + args[0]](message, args, kwargs);
+          } else {
+            this.command(message, args, kwargs);
+          }
+        } else {
+          message.reply("You are not authorized to run this command.");
+        }
+      }
+    }
+  }
+
+  on_message(message) {
+    this._execCommand(message);
+  }
+
+  _get_save_path() {
+    return this.client.path + "\\saves\\" + this.name.toLowerCase() + "\\"
+  };
+
+  load(name) {
+    var string = fs.readFileSync(this._get_save_path() + name + ".json");
+    return JSON.parse(string);
+  }
+
+  save(name, data) {
+    var string = JSON.stringify(data);
+    if (!fs.existsSync(this._get_save_path())) fs.mkdirSync(this.get_save_path());
+    fs.writeFile(this._get_save_path() + name + ".json", string, err => {if (err != null) console.log(err)});
+    console.log(this.name + " Data Saved");
+  }
+
+  save_exists(name) {
+    return fs.existsSync(this._get_save_path() + name + ".json");
+  }
+}
+
+module.exports = exports = {Base}
--- bot.js
+++ bot.js
@@ -1,25 +1,53 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const {Client, MessageEmbed} = require('discord.js');
+const client = new Client();
 
-const _prefix = ";"
+require('dotenv').config();
+require('toml-require').install();
+
+const fs = require('fs');
+const toml = require('toml');
+const concat = require('concat-stream');
+
+client.config = require("./config.toml");
+client.modules = {};
+client.path = module.path;
 
 client.on('ready', () => {
-  console.log('I am ready!');
+  (async () => {
+    try {
+      const files = await fs.promises.readdir('./modules');
+      for (const file of files) {
+        const path = './modules/' + file
+        const stat = await fs.promises.stat(path);
+
+        if (stat.isDirectory()) {
+          const mainfile = require(path + '/main.js');
+          const mod = new mainfile.MainClass(client);
+          client.modules[mod.name.toLowerCase()] = mod;
+          console.log("Loaded module " + mod.name + " (" + mod.description + "), command text: " + client.config.prefix + mod.command_text + "");
+        }
+      }
+    } catch(e) {
+        console.error("Failed to load modules: ", e);
+    }
+  })();
 });
 
 client.on('message', message => {
-  if (message.content.startsWith(_prefix)) {
-    var args = message.content.substring(1).split(" ");
-    var cmd = args.shift();
-
-    switch(cmd) {
-      case 'ping':
-        message.reply('pong');
-        break;
-      default:
-        message.reply('unknown command')
+  Object.values(client.modules).forEach((element) => {
+    try {
+      element.on_message(message);
+    } catch(e) {
+      message.channel.send(new MessageEmbed()
+        .setTitle("Error caused by " + element.name + " module")
+        .setColor(0xff0000)
+        .setDescription("```js\n" + e.stack + "```")
+        .setFooter("This message will be deleted in one minute")
+      ).then(message => {
+        message.delete({ timeout: 60000 });
+      });
     }
-  }
+  });
 });
 
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN).catch(console.error);
