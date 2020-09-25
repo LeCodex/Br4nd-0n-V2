c85a897fcef73884d9a0aa7ae38f8cee4a23c005
Le Codex <40335983+LeCodex@users.noreply.github.com>
2020-09-25 17:23:35 +0200

    [mod-stats] Initial commit

---
+++ modules/stat/main.js
@@ -0,0 +1,59 @@
+const {MessageEmbed} = require('discord.js');
+const {Base} = require(module.parent.path + "/base/main.js");
+const StatLogic = require("./stat_logic.js")
+
+class MainClass extends Base {
+  constructor(client) {
+    super(client);
+    this.name = "Stats";
+    this.description = "Stores scoreboards for everything and anything";
+    this.help = {
+      "": "Sends a list of all created scoreboards",
+      "<scoreboard>": "Sends the requested scoreboard. Use sort=<criteria> to get a sorted list",
+      "<scoreboard> edit=true": "Opens the scoreboard in edit mode"
+    };
+    this.command_text = "stats";
+    this.color = 0x990099;
+
+    if (!this.save_exists("stats")) this.save("stats", {});
+    this.stats = this.load("stats");
+    this.statLogics = [];
+  }
+
+  command(message, args, kwargs) {
+    if (args.length) {
+      if (this.statLogics[message.author.id]) {
+        this.statLogics[message.author.id].close().then(() => this.openNewScoreboard(message, args, kwargs))
+      } else {
+        this.openNewScoreboard(message, args, kwargs)
+      }
+    } else {
+      var embed = new MessageEmbed().setTitle("[STATS] Stored Scoreboards").setColor(this.color);
+      var description = [];
+      for (var [key, value] of Object.entries(this.stats[message.guild.id])) {
+        description.push(value.emoji + " " + key);
+      }
+
+      if (!description.length) description = ["❌ No scoreboards created"]
+      embed.description = description.join("\n");
+      message.reply(embed);
+    }
+  }
+
+  openNewScoreboard(message, args, kwargs) {
+    var name = args.map(e => e = e.substr(0, 1).toUpperCase() + e.substr(1).toLowerCase()).join(" ");
+    if (!this.stats[message.guild.id]) this.stats[message.guild.id] = {};
+    if (!this.stats[message.guild.id][name]) {
+      this.stats[message.guild.id][name] = {name: name, emoji : "🚫", criterias: [], scores: {}};
+      this.save("stats", this.stats);
+      kwargs.edit = true;
+    }
+
+    var scoreboard = this.stats[message.guild.id][name];
+    var embed = new MessageEmbed().setTitle("[STATS] " + name).setColor(this.color);
+
+    this.statLogics[message.author.id] = new StatLogic(this, message.author, scoreboard, message, embed, !kwargs.edit);
+  }
+}
+
+module.exports = exports = {MainClass}
