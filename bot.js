026fd445a49e4e424b0b57b02373f8ec389d56bc
Le Codex <40335983+LeCodex@users.noreply.github.com>
2020-10-07 21:40:03 +0200

    Added getUserFromMention method to the client

--- bot.js
+++ bot.js
@@ -73,6 +73,20 @@ client.error = function(channel, name, e) {
   });
 }
 
+client.getUserFromMention = function(mention) {
+	if (!mention) return;
+
+	if (mention.startsWith('<@') && mention.endsWith('>')) {
+		mention = mention.slice(2, -1);
+
+		if (mention.startsWith('!')) {
+			mention = mention.slice(1);
+		}
+
+		return client.users.cache.get(mention);
+	}
+}
+
 client.login(process.env.BOT_TOKEN).catch(console.error);
 
 const http = require('http');
