43b80c57c182bc1b380d0771afd5ab729043cc59
Le Codex <40335983+LeCodex@users.noreply.github.com>
2020-10-19 15:08:57 +0200

    [mod-montpartasse] Fixed GolenCup

--- modules/montpartasse/cups.js
+++ modules/montpartasse/cups.js
@@ -139,6 +139,7 @@ class GoldenCup extends Cup {
 
 	effect(game, index) {
 		game.players[game.lastPlayed].draw(game, 1);
+		game.players[game.lastPlayed].sendHand();
 
 		super.effect(game, index, "⏫ " + game.players[game.lastPlayed].user.toString() + " a pioché une tasse! ⏫");
 	}
