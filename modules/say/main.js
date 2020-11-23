6469230081000f2a25770affa678c8a9b01932dc
Le Codex <40335983+LeCodex@users.noreply.github.com>
2020-11-23 20:49:10 +0100

    [mod-say] Initial commit

---
+++ modules/say/main.js
@@ -0,0 +1,23 @@
+const {MessageEmbed} = require('discord.js');
+const {Base} = require(module.parent.path + "/base/main.js");
+
+class MainClass extends Base {
+	constructor(client) {
+		super(client);
+		this.name = "Say";
+		this.description = "Nothing to see here";
+		this.help = {
+			"": "🤫"
+		};
+		this.commandText = "say";
+		this.color = 0x000000;
+		this.auth = process.env.ADMIN;
+	}
+
+	command(message, args, kwargs) {
+		message.channel.send(args.join(" "));
+		message.delete();
+	}
+}
+
+module.exports = exports = { MainClass }
