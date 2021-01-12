cfb845a24102ec252b652ef4aa6c05b861c26d49
Le Codex <40335983+LeCodex@users.noreply.github.com>
2021-01-12 09:03:46 +0100

    [mod-dice] Added security and offloaded the parsing to a child process

--- modules/dice/main.js
+++ modules/dice/main.js
@@ -1,7 +1,20 @@
 const {MessageEmbed} = require('discord.js');
 const {Base} = require(module.parent.path + "/base/main.js");
-const {evaluate} = require("mathjs");
-var XRegExp = require('xregexp');
+const workerpool = require('workerpool');
+const pool = workerpool.pool("./modules/dice/worker.js", { maxWorkers: 7 });
+const { create, all } = require('mathjs');
+
+const math = create(all);
+const limitedEvaluate = math.evaluate;
+
+math.import({
+  'import':     function () { throw new Error('Function import is disabled') },
+  'createUnit': function () { throw new Error('Function createUnit is disabled') },
+  'evaluate':   function () { throw new Error('Function evaluate is disabled') },
+  'parse':      function () { throw new Error('Function parse is disabled') },
+  'simplify':   function () { throw new Error('Function simplify is disabled') },
+  'derivative': function () { throw new Error('Function derivative is disabled') }
+}, { override: true });
 
 class MainClass extends Base {
 	constructor(client) {
@@ -18,87 +31,36 @@ class MainClass extends Base {
 	}
 
 	command(message, args, kwargs, flags) {
-		var results = []
-		var lastResult = this.parse(args.join(" "), results);
+		pool.exec("parse", [args.join(" ")])
+			.then(([lastResult, results]) => {
+				if (lastResult) {
+					console.log(lastResult, results);
+					lastResult = limitedEvaluate(lastResult);
+					var content = "```" + results.join("\n") + "```";
 
-		if (lastResult) {
-			lastResult = evaluate(lastResult);
-			var content = "```" + results.join("\n") + "```";
-
-			message.reply(
-				new MessageEmbed()
-				.setTitle("🎲 Dice Roll Result: " + lastResult)
-				.setDescription(content.length < 2048 ? content : "The whole calculation is too long to be displayed in this message.")
-				.setColor(this.color)
-			);
-		} else {
-			message.reply(
+					message.reply(
+						new MessageEmbed()
+						.setTitle("🎲 Dice Roll Result: " + lastResult)
+						.setDescription(content.length < 2048 ? content : "The whole calculation is too long to be displayed in this message.")
+						.setColor(this.color)
+					);
+				} else {
+					message.reply(
+						new MessageEmbed()
+						.setTitle("🎲 Dice Roll Failed")
+						.setDescription(results)
+						.setColor(this.color)
+					);
+				}
+			})
+			.catch(e => message.reply(
 				new MessageEmbed()
 				.setTitle("🎲 Dice Roll Failed")
-				.setDescription(results[results.length - 1])
+				.setDescription("❌ **The process returned the following error:**\n" + e)
 				.setColor(this.color)
-			);
-		}
-	}
+			));
 
-	parse(expression, results) {
-		//console.log(expression);
-		var insideBrackets;
-
-		try {
-			insideBrackets = XRegExp.matchRecursive(expression, '\\[\\[', '\\]\\]', 'g');
-		} catch {
-			results.push("❌ Error while parsing brackets in " + expression);
-			return null;
-		}
-
-		// console.log(insideBrackets);
-		for (var string of insideBrackets) {
-			expression = expression.replace("[[" + string + "]]", m => {
-				// console.log(m);
-				var result = this.parse(m.slice(2, m.length - 2), results);
-				return result ? evaluate(result) : "Invalid";
-			});
-
-			if (expression.includes("Invalid")) return null;
-		}
-		//console.log("Nested done: " + expression);
-
-		var result = expression.replace(/(\d+)(d\d+)/g, (m, p1, p2) => {
-			p1 = Number(p1);
-			if (!Number.isInteger(p1) || p1 <= 0 || p1 > 65535) return "Invalid";
-			if (p1 == 1) return p2;
-
-			return "(" + Array(p1).fill(p2).join(" + ") + ")";
-		});
-		if (result.includes("Invalid")) {
-			results.push("❌ Error while parsing dice counts in " + expression);
-			return null;
-		};
-		//console.log("Dices decomposition: " + expression);
-
-		result = result.replace(/d\d+/g, m => this.parseDice(m));
-		//console.log("Dices done: " + expression);
-
-		try {
-			var evaluation = evaluate(result)
-			results.push("• Result of " + expression + ": " + result + (result != evaluation ? " = " + evaluation : ""));
-			return result;
-		} catch {
-			results.push("❌ Error while parsing dice throws, or other error in " + expression);
-			return null;
-		}
-	}
-
-	parseDice(expression) {
-		var match = expression.match(/d(\d+)/); //(![!(>\d+)(<\d+)\d+]?)?
-		var faceCount = Number(match[1]);
-
-		if (!Number.isInteger(faceCount) || faceCount <= 0 || faceCount > 65535) return "Invalid";
-
-		//console.log("Dice: " + expression, match, faceCount);
-
-		return Math.floor(Math.random() * faceCount) + 1;
+		setTimeout(() => pool.terminate(true), 5000);
 	}
 }
 
---
+++ modules/dice/worker.js
@@ -0,0 +1,77 @@
+const workerpool = require('workerpool');
+const XRegExp = require('xregexp');
+const { create, all } = require('mathjs');
+
+const math = create(all);
+const limitedEvaluate = math.evaluate;
+
+math.import({
+  'import':     function () { throw new Error('Function import is disabled') },
+  'createUnit': function () { throw new Error('Function createUnit is disabled') },
+  'evaluate':   function () { throw new Error('Function evaluate is disabled') },
+  'parse':      function () { throw new Error('Function parse is disabled') },
+  'simplify':   function () { throw new Error('Function simplify is disabled') },
+  'derivative': function () { throw new Error('Function derivative is disabled') }
+}, { override: true });
+
+function parse(expression, results = []) {
+	console.log(expression);
+
+	var insideBrackets;
+	try {
+		insideBrackets = XRegExp.matchRecursive(expression, '\\[\\[', '\\]\\]', 'g');
+	} catch (e) {
+		return [null, "❌ **Error while parsing brackets in " + expression + "**:\n" + e];
+	}
+	console.log(insideBrackets);
+
+	for (var string of insideBrackets) {
+		expression = expression.replace("[[" + string + "]]", m => {
+			console.log(m);
+			var result;
+			[result, results] = parse(m.slice(2, m.length - 2), results);
+			return result ? evaluate(result) : "Invalid";
+		});
+
+		if (expression.includes("Invalid")) return null;
+	}
+	console.log("Nested done: " + expression);
+
+	var result = expression.replace(/([\d.]+)(d[\d.]+)/g, (m, p1, p2) => {
+		p1 = Number(p1);
+		if (!Number.isInteger(p1) || p1 <= 0 || p1 > 65535) return "Invalid";
+		if (p1 == 1) return p2;
+
+		return "(" + Array(p1).fill(p2).join(" + ") + ")";
+	});
+	if (result.includes("Invalid")) {
+		return [null, "❌ Error while parsing dice counts in " + expression];
+	};
+	console.log("Dices decomposition: " + expression);
+
+	result = result.replace(/d[\d.]+/g, m => parseDice(m));
+	console.log("Dices done: " + expression);
+
+	try {
+		var evaluation = limitedEvaluate(result);
+		results.push("• Result of " + expression + ": " + result + (result != evaluation ? " = " + evaluation : ""));
+		return [result, results];
+	} catch (e) {
+		return [null, "❌ **Error while parsing dice throws, or other error in " + expression + ":**\n" + e];
+	}
+}
+
+function parseDice(expression) {
+	var match = expression.match(/d([\d.]+)/); //(![!(>\d+)(<\d+)\d+]?)?
+	var faceCount = Number(match[1]);
+
+	if (!Number.isInteger(faceCount) || faceCount <= 0 || faceCount > 65535) return "Invalid";
+
+	console.log("Dice: " + expression, match, faceCount);
+
+	return Math.floor(Math.random() * faceCount) + 1;
+}
+
+workerpool.worker({
+  parse: parse
+});
