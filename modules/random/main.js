758e7cee4e340f7fa35c8a3c74f88ad580c0ffb3
Le Codex <40335983+LeCodex@users.noreply.github.com>
2021-01-17 20:50:30 +0100

    [mod-random] Added 8ball subcommand

--- modules/random/main.js
+++ modules/random/main.js
@@ -120,6 +120,36 @@ class MainClass extends Base {
 		args.splice();
 		this.reply(message, "🔮 I choose **" + args[Math.floor(Math.random() * args.length)] + "**!");
 	}
+
+	com_8ball(message, args, kwargs, flags) {
+		var answers = [
+			"It is certain.",
+			"It is decidedly so.",
+			"Without a doubt.",
+			"Yes – definitely.",
+			"You may rely on it.",
+
+			"As I see it, yes.",
+			"Most likely.",
+			"Outlook good.",
+			"Yes.",
+			"Signs point to yes.",
+
+			"Reply hazy, try again.",
+			"Ask again later.",
+			"Better not tell you now.",
+			"Cannot predict now.",
+			"Concentrate and ask again.",
+
+			"Don't count on it.",
+			"My reply is no.",
+			"My sources say no.",
+			"Outlook not so good.",
+			"Very doubtful."
+		];
+
+		this.reply(message, "🎱 " + answers[Math.floor(Math.random() * answers.length)]);
+	}
 }
 
 module.exports = exports = {MainClass};
