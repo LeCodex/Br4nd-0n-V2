27343066ffd5803544dc6fc9e131e8915f54f463
Le Codex <40335983+LeCodex@users.noreply.github.com>
2021-06-30 19:57:55 +0200

    [mod-petri] Added rules

--- modules/petri/main.js
+++ modules/petri/main.js
@@ -62,7 +62,29 @@ class MainClass extends Base {
 	}
 
 	com_rules(message, args, kwargs, flags) {
+		message.reply(
+			new MessageEmbed()
+			.setTitle(":small_orange_diamond: Règles de Petri :small_orange_diamond:")
+			.setDescription(`
+**:small_blue_diamond: But du jeu : :small_blue_diamond:**
+Chaque joueur commence avec une troupe à un endroit aléatoire de la carte de 10x10 au début de la partie.
+Le gagnant est le joueur qui est le dernier avec des troupes encore vivantes, ou bien qui arrive à contrôler 50% de la carte.
+Après 40 tours de table complets (manche) sans qu'un gagnant ne soit déterminé, le joueur avec le plus de troupes gagne.
 
+**:small_blue_diamond: Déroulement d’un tour : :small_blue_diamond:**
+- Le joueur choisit une direction
+- Toutes ses troupes essaient de se répliquer dans cette direction:
+--- Si la case dans cette direction est vide, une nouvelle troupe de sa couleur est créée
+--- Si la case est occupée par une troupe alliée, rien ne se passe
+--- Si la case est occupée par une troupe ennemie, un combat se déclenche entre cette troupe et celle qui essaie de se répliquer
+
+**:small_blue_diamond: Les combats : :small_blue_diamond:**
+Pour déterminer qui gagne le combat, il suffit de regarder le nombre de troupes alliées se trouvant en une ligne derrière les deux troupes:
+- Si l'attaquant en a le plus, il se réplique sur le défenseur
+- Si le défenseur en a le plus, rien ne se passe
+- S'il y a égalité, le défenseur est tué mais l'attaquant ne se réplique pas`)
+			.setColor(this.color)
+		)
 	}
 
 	com_debug(message, args, kwargs, flags) {
