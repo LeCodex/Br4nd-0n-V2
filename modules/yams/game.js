839e3dcaab60cc7b195d806100e71316a5def322
Le Codex <40335983+LeCodex@users.noreply.github.com>
2021-09-26 10:42:07 +0200

    [mod-yams] Fixed sequences

--- modules/yams/game.js
+++ modules/yams/game.js
@@ -77,6 +77,7 @@ class Game {
 						max_number = -1;
 					}
 				}
+				if (min_number != -1) sequences.push(sequence);
 
 				var totals = sequences.map(e => e.reduce((a, f) => a + f + 1, 0));
 				totals.sort();
@@ -95,6 +96,7 @@ class Game {
 						min_number = -1;
 					}
 				}
+				if (min_number != -1) sequences.push(sequence);
 
 				var totals = sequences.map(e => e.reduce((a, f) => a + f + 1, 0));
 				totals.sort();
