98902cc150a6295baadc1e6cad110aa519d19109
Le Codex <40335983+LeCodex@users.noreply.github.com>
2022-01-26 15:08:56 +0100

    [mod-coupdjus] Fixed points when finishing player has recipe

--- modules/coupdjus/game.js
+++ modules/coupdjus/game.js
@@ -170,8 +170,8 @@ class Game {
 				var used = false;
 				for (var ply of Object.values(this.players)) {
 					if (ply.recipes.includes(recipe)) {
-						gains[ply.user.id] = gains[ply.user.id] ? gains[ply.user.id] + 1 : 1;
 						gains[blender[2].player.user.id] = 1;
+						gains[ply.user.id] = gains[ply.user.id] ? gains[ply.user.id] + 1 : 1;
 						used = true;
 						summary.push(ply.user.toString() + " a cette recette!");
 					}
